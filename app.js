950e17387448ba8820fbef9f4eb3e35546624a47
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-01-27 18:54:25 +0530

    send email

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 // const url = "https://www.amazon.in/ZEBRONICS-Zeb-Storm-Microphone-Adjustable-Lightweight/dp/B084BVXCBF/"
-
+require('dotenv').config()
 const nightmare = require('nightmare')
+const nodemailer = require('nodemailer')
 
 const args = process.argv.slice(2)
 const url = args[0]
 const idealPrice = args[1]
+EMAIL_ID = process.env.EMAIL_ID
+EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL_ID,
+    pass: EMAIL_PASSWORD,
+  },
+});
+
+var mailOptions = {
+  from: EMAIL_ID,
+  to: "hackrgpv@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
 
 priceChecker()
 async function priceChecker() {
